a56297b3dc6ffa612bd18bc2355d1fca63f86b98
Trinity Egbukwu <108954089+Chloetrini@users.noreply.github.com>
2022-07-15 22:12:40 +0000

    algorithm

--- replit.nix
+++ replit.nix
@@ -1,6 +1,9 @@
 { pkgs }: {
 	deps = [
-		pkgs.nodejs-16_x
+		pkgs.yarn
+pkgs.yarn
+pkgs.yarn test
+pkgs.nodejs-16_x
         pkgs.nodePackages.typescript-language-server
         pkgs.yarn
         pkgs.replitPackages.jest
